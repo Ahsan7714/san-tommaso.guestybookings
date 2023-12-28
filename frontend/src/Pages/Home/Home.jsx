89fa6a7316468872b9b73e6c5b1fe718bdfbdd50
AHSAN <115989038+Ahsan7714@users.noreply.github.com>
2023-12-29 00:15:59 +0500

    completed

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -8,11 +8,11 @@ const Home = () => {
     <div>
       <div>
         <img src={bg} alt="" className='relative h-[100vh] lg:h-[120vh] w-full object-cover' />
-        <div className='absolute lg:top-[60%] text-white lg:left-[28%] top-[40%] text-center'>
+        <div className='absolute lg:top-[13%] text-white lg:left-[25%] top-[40%] text-center'>
         <h1 className='text-[40px] font-bold'>Booking Agriturismo San Tommaso</h1>
         <p className='text-[25px] font-semibold absolute lg:left-[40%] left-[29%]'>Enjoy your stay</p>
         </div>
-        <div className='absolute lg:left-[7%] lg:top-[24%] top-[20%] left-[10%]'>
+        <div className=''>
         <div className='  -translate-y-16  shadow-2xl w-fit mx-auto'>
           <DatePicker/>
         </div>
