ae134708ecabc235c8fb069215a74bfe701533d1
AQIB-NAWAB <aqibnawab1100@gmail.com>
2024-01-01 11:08:46 +0500

    changed the url

--- frontend/src/context/baseUrl.jsx
+++ frontend/src/context/baseUrl.jsx
@@ -1,4 +1,4 @@
-const  baseUrl = "https://booking-backend-lac.vercel.app/api/v1";
+const  baseUrl = "https://booking-backend-rho.vercel.app/api/v1";
 // const baseUrl="http://localhost:8080/api/v1"
 
 export default baseUrl
