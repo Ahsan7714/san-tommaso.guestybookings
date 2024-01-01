8c23b123f0240eb19c72bfbe0ed96a45d9f4e1b3
AQIB-NAWAB <aqibnawab1100@gmail.com>
2024-01-01 19:52:31 +0500

    done

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 require("dotenv").config({ path: "./config/.env" });
 app.use(cors(
     {
-        origin:"https://booking-ecru-phi.vercel.app",
+        origin:"https://booking-through-aqib.vercel.app",
         // origin:"http://localhost:5173",
         withCredentials:true,
         credentials:true
