7863229f85da12cd04462f4e861946278a1df8f4
AQIB-NAWAB <aqibnawab1100@gmail.com>
2024-01-02 22:12:41 +0500

    all done

--- frontend/src/context/baseUrl.jsx
+++ frontend/src/context/baseUrl.jsx
@@ -1,4 +1,4 @@
-const  baseUrl = "https://booking-backend-beta.vercel.app/api/v1";
+const  baseUrl = "https://booking-backend-three.vercel.app/api/v1";
 // const baseUrl="http://localhost:8080/api/v1"
 
 export default baseUrl
