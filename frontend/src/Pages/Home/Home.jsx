6588db11d3cc1bdabcc87ba16762d7eadb7a3636
AQIB-NAWAB <aqibnawab1100@gmail.com>
2024-02-10 13:10:55 +0500

    done

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -6,28 +6,26 @@ import { useEffect } from 'react'
 import { useLocalContext } from '../../context/contextProvider'
 
 const Home = () => {
-  const {getProperties}=useLocalContext()
+  const { getProperties } = useLocalContext()
   useEffect(() => {
     getProperties();
   }, []); 
    return (
     <div>
-      <div className=' font-poppins'>
+      <div className='font-poppins relative'>
         <div className='bg-[#00000040] z-10'>
-        <img src={"./images/IMG_4153.jpg"} alt="" className='relative h-[70vh] lg:h-[100vh] w-full object-cover -z-10' />
+          <img src={"./images/IMG_4153.jpg"} alt="" className='relative h-[70vh] lg:h-[100vh] w-full object-cover -z-10' />
         </div>
-        <div className='absolute lg:top-[50%] text-white lg:left-[23%] top-[29%] text-center'>
-        <h1 className='text-[40px] font-bold'>Booking Agriturismo San Tommaso</h1>
-        <p className='text-[25px] font-semibold absolute lg:left-[40%] left-[29%]'>Enjoy your stay</p>
+        <div className='z-40 absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 text-white text-center'>
+          <h1 className='text-[40px] font-bold'>Booking Agriturismo San Tommaso</h1>
+          <p className='text-[25px] font-semibold absolute lg:left-[40%] left-[29%]'>Enjoy your stay</p>
         </div>
-        <div className=''>
-        <div className='  -translate-y-16  shadow-2xl w-fit mx-auto'>
+        <div className='-translate-y-16 shadow-2xl w-fit mx-auto'>
           <DatePicker source={"Home"}/>
         </div>
       </div>
       <About/>
       <Contact/>
-    </div>
     </div>
   )
 }
